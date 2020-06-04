9413ba603971fce858b5e71e3a27272eeff5ea2b
Yuta Saito <uc4w6c@bma.biglobe.ne.jp>
2020-06-05 00:25:20 +0900

    162D問題まで

--- atcoder/past/20200604_162/d/main.cpp
+++ atcoder/past/20200604_162/d/main.cpp
@@ -1,6 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// 以下だとタイムアウトする
+// D - RGB Triplets
 int main() {
+    int N;
+    string S;
+    cin >> N >> S;
 
+    if (N < 3) {
+        cout << 0;
+        return 0;
+    }
+    int sum = 0;
+    for (int i = 0; i < N - 2; i++) {
+        for (int j = i + 1; j < N - 1; j++) {
+            for (int k = j + 1; k < N; k++) {
+                if (S[i] == S[j]) break;
+                if (S[i] == S[k]) continue;
+                if (S[j] == S[k]) continue;
+
+                if ((j - i) != (k - j)) {
+                    // cout << "i:" << i << S[i] << ", j:" << j << S[j] << ", k:" << k << S[k] << endl;
+                    sum++;
+                }
+            }
+        }
+    }
+    cout << sum;
 }
---
+++ atcoder/past/20200604_162/d/main2.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// D - RGB Triplets
+int main() {
+    int N;
+    string S;
+    cin >> N >> S;
+
+    if (N < 3) {
+        cout << 0;
+        return 0;
+    }
+
+    int r = 0, g = 0, b = 0;
+    for (int i = 0; i < N; i++) {
+        if (S[i] == 'R') r++;
+        if (S[i] == 'G') g++;
+        if (S[i] == 'B') b++;
+    }
+
+    // long long all = (long long)r * g * b;
+
+    int minus = 0;
+    for (int i = 0; i < N - 2; i++) {
+        for (int j = i + 1; j < N - 1; j++) {
+            if (j + (j - i) >= N) break;
+            if (S[i] != S[j] && S[i] != S[j + (j - i)] && S[j + (j - i)] != S[j]) {
+                // cout << "i:" << i << S[i] << ", j:" << j << S[j] << ", k:" << j + (j - i) << S[j + (j - i)] << endl;
+                minus++;
+                // all--;
+            }
+        }
+    }
+
+    // long long all = (long long)r * g * b;
+    // cout << all - minus;
+    cout << ((long long)(r * g * b)) - minus;
+}
