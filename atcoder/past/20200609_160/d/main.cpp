e7d2006e27252f7c54ae5d015efe52fd0159a314
Yuta Saito <uc4w6c@bma.biglobe.ne.jp>
2020-06-10 21:45:07 +0900

    160 D問題

--- atcoder/past/20200609_160/d/main.cpp
+++ atcoder/past/20200609_160/d/main.cpp
@@ -2,6 +2,36 @@
 using namespace std;
 
 // D - Line++
+#define MAX 2000
+int ans[MAX];
+int N, X, Y;
+
+void calc(int nowPoint) {
+    if (nowPoint == N) {
+        return;
+    }
+    for (int i = nowPoint + 1; i < N; i++) {
+        // cout << "(" << nowPoint + 1 << ", " << i + 1 << ")";
+        
+        // これだと戻るケースが考慮されていない X -> Y - 1 みたいな
+        if (nowPoint <= X && Y <= i) {
+            // cout << " : " << (X - nowPoint) + (i - Y);
+            ans[(X - nowPoint) + (i - Y) + 1]++;
+        } else {
+            // cout << " : " << i - nowPoint;
+            ans[i - nowPoint]++;
+        }
+        // cout << endl;
+    }
+    calc(nowPoint + 1);
+}
+
 int main() {
-    
+    cin >> N >> X >> Y;
+    X--;
+    Y--;
+    calc(0);
+    for (int i = 1; i < N; i++) {
+        cout << ans[i] << endl;
+    }
 }
