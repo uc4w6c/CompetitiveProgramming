dc6040dcea11edd04767a113cf8029e4c76d2844
Yuta Saito <uc4w6c@bma.biglobe.ne.jp>
2020-06-11 07:53:11 +0900

    160 D問題完了

--- atcoder/past/20200609_160/d/main.cpp
+++ atcoder/past/20200609_160/d/main.cpp
@@ -11,25 +11,29 @@ void calc(int nowPoint) {
         return;
     }
     for (int i = nowPoint + 1; i < N; i++) {
+        // cout << "nowPoint:" << nowPoint << ", X:" << X << endl;
+        // cout << "i:" << i << ", Y:" << Y << endl;
+
+        // cout << "(" << nowPoint << ", " << i << ")";
         // cout << "(" << nowPoint + 1 << ", " << i + 1 << ")";
-        
+
         // これだと戻るケースが考慮されていない X -> Y - 1 みたいな
         // 開始地点 / 終了地点が X Yを跨ぐ
         if (nowPoint <= X && Y <= i) {
-            // cout << " : " << (X - nowPoint) + (i - Y);
+            // cout << " A: " << (X - nowPoint) + (i - Y) + 1;
             ans[(X - nowPoint) + (i - Y) + 1]++;
 
         // 開始地点 / 終了地点が X Yの間ではない
         } else if (i <= X || Y <= nowPoint) {
-            // cout << " : " << i - nowPoint;
+            // cout << " B: " << i - nowPoint;
             ans[i - nowPoint]++;
 
         // 開始地点 or 終了地点が X Yの間にあるとき
         } else {
             // ちょっと一旦雑に計算
-            // 間違っている
             int minRoute = i - nowPoint;
-            minRoute = (abs(nowPoint - X) + 1 + abs(i - Y) ,minRoute);
+            minRoute = min(abs(nowPoint - X) + 1 + abs(i - Y) ,minRoute);
+            // cout << " : " << minRoute;
             // 開始地点がX 以上である場合
             /*if (nowPoint > X) {
                 minRoute = ((nowPoint - X) + 1 + abs(i - Y) ,minRoute)
@@ -47,6 +51,7 @@ int main() {
     cin >> N >> X >> Y;
     X--;
     Y--;
+    // cout << "X:" << X << ", Y:" << Y << endl;
     calc(0);
     for (int i = 1; i < N; i++) {
         cout << ans[i] << endl;
