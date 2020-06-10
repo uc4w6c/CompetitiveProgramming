31275416b6935fde30ea425e00a4c90cfdf3694f
Yuta Saito <uc4w6c@bma.biglobe.ne.jp>
2020-06-10 23:57:23 +0900

    160 D問題途中g

--- atcoder/past/20200609_160/d/main.cpp
+++ atcoder/past/20200609_160/d/main.cpp
@@ -14,12 +14,29 @@ void calc(int nowPoint) {
         // cout << "(" << nowPoint + 1 << ", " << i + 1 << ")";
         
         // これだと戻るケースが考慮されていない X -> Y - 1 みたいな
+        // 開始地点 / 終了地点が X Yを跨ぐ
         if (nowPoint <= X && Y <= i) {
             // cout << " : " << (X - nowPoint) + (i - Y);
             ans[(X - nowPoint) + (i - Y) + 1]++;
-        } else {
+
+        // 開始地点 / 終了地点が X Yの間ではない
+        } else if (i <= X || Y <= nowPoint) {
             // cout << " : " << i - nowPoint;
             ans[i - nowPoint]++;
+
+        // 開始地点 or 終了地点が X Yの間にあるとき
+        } else {
+            // ちょっと一旦雑に計算
+            // 間違っている
+            int minRoute = i - nowPoint;
+            minRoute = (abs(nowPoint - X) + 1 + abs(i - Y) ,minRoute);
+            // 開始地点がX 以上である場合
+            /*if (nowPoint > X) {
+                minRoute = ((nowPoint - X) + 1 + abs(i - Y) ,minRoute)
+            } else {
+                minRoute = (abs(nowPoint - X) + 1 + abs(i - Y) ,minRoute)
+            }*/
+            ans[minRoute]++;
         }
         // cout << endl;
     }
