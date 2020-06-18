a6b276b91e4d661f03244ffa35671ed03e36ca73
Yuta Saito <uc4w6c@bma.biglobe.ne.jp>
2020-06-18 23:50:50 +0900

    158 C問題

--- atcoder/past/20200618_158/c/main.cpp
+++ atcoder/past/20200618_158/c/main.cpp
@@ -1,7 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// 
+// C - Tax Increase
 int main() {
-    
+    int A, B;
+    cin >> A >> B;
+
+    int X = B * 10;
+    int result = -1;
+    int tmp;
+    for (int i = X; i <= X + 10; i++) {
+        // cout << i << " : "; // debug
+        tmp = i * 0.1;
+        // cout << tmp << ", "; // debug
+        if (tmp != B) break;
+        tmp = i * 0.08;
+        // cout << tmp << ", " << endl; // debug
+        if (tmp == A) {
+            result = i;
+            break;
+        }
+    }
+    cout << result;
 }
---
+++ atcoder/past/20200618_158/c/test.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// C - Tax Increase
+int main() {
+    for (int i = 100; i <= 200; i++) {
+        cout << i << " * 0.08 = " << i * 0.08 << endl;
+        cout << i << " * 0.1 = " << i * 0.1 << endl;
+    }
+}
