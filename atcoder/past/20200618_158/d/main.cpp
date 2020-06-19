37c6cf15bdea2322c6b8372ae045a18c4d34541f
Yuta Saito <uc4w6c@bma.biglobe.ne.jp>
2020-06-19 20:47:47 +0900

    158 D問題

--- atcoder/past/20200618_158/d/main.cpp
+++ atcoder/past/20200618_158/d/main.cpp
@@ -1,7 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// 
+// D - String Formation
 int main() {
-    
+    string S;
+    cin >> S;
+    list<char> result;
+    for (int i = 0; i < S.size(); i++) {
+        result.push_back(S[i]);
+    }
+
+    int Q;
+    cin >> Q;
+
+    bool isReverse = false;
+    bool isFrontAdd = false;
+    int T, F;
+    char C;
+    for (int i = 0; i < Q; i++) {
+        cin >> T;
+        if (T == 1) {
+            isReverse = !isReverse;
+        } else {
+            cin >> F;
+            cin >> C;
+            if (F == 1) {
+                isFrontAdd = !isReverse;
+            } else {
+                isFrontAdd = isReverse;
+            }
+            if (isFrontAdd) {
+                result.push_front(C);
+            } else {
+                result.push_back(C);
+            }
+        }
+    }
+
+    while (!result.empty()) {
+        if (isReverse) {
+            cout << result.back();
+            result.pop_back();
+        } else {
+            cout << result.front();
+            result.pop_front();
+        }
+    }
 }
---
+++ atcoder/past/20200618_158/d/test.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// D - String Formation
+// test
+int main() {
+    list<char> test;
+    test.push_back('a');
+    test.push_back('b');
+    test.push_front('c');
+    for (auto x: test) {
+        cout << x;
+    }
+
+}
