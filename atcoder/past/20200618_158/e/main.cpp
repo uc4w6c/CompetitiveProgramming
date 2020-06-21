eecd069243896fcc6c75f08362fec2374069e21b
Yuta Saito <uc4w6c@bma.biglobe.ne.jp>
2020-06-21 09:29:06 +0900

    11章完了

---
+++ atcoder/past/20200618_158/e/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// E - Divisible Substring
+// まずはTLEしそうなパターン
+int main() {
+    int N, S, P;
+    cin >> N >> S >> P;
+
+    for (int i = P; i <= S; i += P) {
+        // i の桁数を取得してそれずつズラす
+    }
+
+}
---
+++ atcoder/past/20200618_158/e/test.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// E - Divisible Substring
+int main() {
+    int max = pow(10, 5) * 2;
+
+    cout << max / 2;
+}
+// 100,000
+// 10
+// 2 4 6 8
+
+// 2 - 1 0
+// 4 - 1 0
+// 6 - 1 0
+// 8 - 1 0
+// 10 - 10
