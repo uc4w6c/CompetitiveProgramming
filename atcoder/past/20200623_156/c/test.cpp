29e52c432fe36a1f6002605bb8aa0bb2b25c035f
Yuta Saito <uc4w6c@bma.biglobe.ne.jp>
2020-06-26 21:43:00 +0900

    156

--- atcoder/past/20200623_156/c/test.cpp
+++ atcoder/past/20200623_156/c/test.cpp
@@ -2,13 +2,11 @@
 using namespace std;
 
 // C - Rally
-// 効率化はしていないが、100件くらいならこれでOK
 int main() {
     int N;
     cin >> N;
     int X[N];
     int minX, maxX;
-    int sumMin = 0;
     for (int i = 0; i < N; i++) {
         cin >> X[i];
         if (i == 0) {
@@ -17,18 +15,15 @@ int main() {
         }
         minX = min(minX, X[i]);
         maxX = max(maxX, X[i]);
-        sumMin += pow(X[i], 2);
     }
-
+    // cout << minX << endl;  // debug
+    // cout << maxX << endl;  // debug
     int sum;
     for (int i = minX; i <= maxX; i++) {
         sum = 0;
         for (int j = 0; j < N; j++) {
-            if ((i - X[j]) != 0) {
-                sum += pow((i - X[j]), 2);
-            }
+            sum += abs(i - X[j]);
         }
-        sumMin = min(sum, sumMin);
+        cout << i << " : " << sum << endl;
     }
-    cout << sumMin;
 }
