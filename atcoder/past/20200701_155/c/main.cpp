8a76d125295ad7e905fb2ae76c51a9f6585b7798
Yuta Saito <uc4w6c@bma.biglobe.ne.jp>
2020-07-03 18:59:45 +0900

    155 C問題

--- atcoder/past/20200701_155/c/main.cpp
+++ atcoder/past/20200701_155/c/main.cpp
@@ -8,13 +8,23 @@ int main() {
     cin >> N;
     map<string, int> betMap;
     string S;
+    int maxCount = 0;
     for (int i = 0; i < N; i++) {
         cin >> S;
         betMap[S]++;
+        maxCount = max(betMap[S], maxCount);
     }
 
+    vector<string> maxBetList;
     for (auto bet : betMap){
-        std::cout << bet.first << " : " << bet.second << "\n";
+        if (bet.second == maxCount) {
+            maxBetList.push_back(bet.first);
+        }
+    }
+    sort(maxBetList.begin(), maxBetList.end());
+
+    for (auto bet : maxBetList) {
+        cout << bet << endl;
     }
 
 }
