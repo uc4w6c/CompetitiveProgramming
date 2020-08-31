7a79ee9aa76bb1386b677096c52a22057f2dd9ff
Yuta Saito <uc4w6c@bma.biglobe.ne.jp>
2020-08-31 23:54:43 +0900

    151

---
+++ atcoder/past/20200831_151/a/main.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 
+int main() {
+    char c;
+    cin >> c;
+    cout << string(1, c + 1);
+}
---
+++ atcoder/past/20200831_151/b/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 
+int main() {
+    int N, K, M;
+    cin >> N >> K >> M;
+    int A[N];
+    int sum = 0;
+    for (int i = 0; i < N - 1; i++) {
+        cin >> A[i];
+        sum += A[i];
+    }
+    
+
+}
