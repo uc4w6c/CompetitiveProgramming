088921089a8a12fbdf22a3893624bff650347578
Yuta Saito <uc4w6c@bma.biglobe.ne.jp>
2020-09-04 22:06:51 +0900

    vectorのテスト

--- atcoder/past/20200903_150/c/test.cpp
+++ atcoder/past/20200903_150/c/test.cpp
@@ -7,6 +7,10 @@ int main() {
     cout << test << endl;
     int test2 = pow(8, 8);
     cout << test2;
+    vector<bool> testList(4, true);
+    for (int i = 0; i < 4; i++) {
+        cout << testList[i];
+    }
 }
 // 16777216
 // 100000000 (10^8)
