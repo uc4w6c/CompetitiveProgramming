d13b07f835720023ac5ea8c985eaeb76e475a16b
Yuta Saito <uc4w6c@bma.biglobe.ne.jp>
2020-09-06 23:52:31 +0900

    149 D

--- atcoder/past/20200906_149/d/main.cpp
+++ atcoder/past/20200906_149/d/main.cpp
@@ -1,7 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// 
+// これだとなぜかwaになる
 int main() {
-    
+    int N, K;
+    int R, S, P;
+    string T;
+    cin >> N >> K;
+    cin >> R >> S >> P;
+    cin >> T;
+
+    long long score = 0;
+    vector<bool> winList(N, false);
+    for (int i = 0; i < N; i++) {
+        if (i >= K) {
+            if (T[i - K] == T[K] && winList[i - K]) {
+                winList[i] = false;
+                // cout << i << endl; // debug
+                continue;
+            }
+        }
+
+        switch (T[i]) {
+            case 'r': {
+                score += P;
+                break;
+            }
+            case 's': {
+                score += R;
+                break;
+            }
+            case 'p': {
+                score += S;
+                break;
+            }
+        }
+        winList[i] = true;
+    }
+    cout << score;
 }
