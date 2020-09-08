e1eff457249c2c88ec4a62e7ebcf7426092d1447
Yuta Saito <uc4w6c@bma.biglobe.ne.jp>
2020-09-08 21:52:47 +0900

    149

--- atcoder/past/20200906_149/e/main.cpp
+++ atcoder/past/20200906_149/e/main.cpp
@@ -1,7 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// 
+// 以下はTLEになる
 int main() {
-    
+    int N;
+    long long M;
+    cin >> N >> M;
+    vector<int> A(N);
+    for (int i = 0; i < N; i++) {
+        cin >> A[i];
+    }
+    priority_queue<int> result;
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            result.push(A[i] + A[j]);
+        }
+    }
+
+    long long happyPoint = 0;
+    for (int i = 0; i < M; i++) {
+        happyPoint += result.top();
+        result.pop();
+    }
+    cout << happyPoint;
 }
---
+++ atcoder/past/20200906_149/e/test.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 
+int main() {
+    long long test = pow(10, 5);
+    test = pow(test, 2);
+    cout << test;
+}
